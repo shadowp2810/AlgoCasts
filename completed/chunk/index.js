fe508265d2793089e426556f30f66f1f216b0cfc
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2022-01-31 20:11:34 +0530

    Write Array Chunking function.

---
+++ completed/chunk/index.js
@@ -0,0 +1,53 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+function chunk(array, size) {
+  const chunked = [];
+
+  for (let element of array) {
+    const last = chunked[chunked.length - 1];
+    if (!last || last.length === size) {
+      chunked.push([element]);
+    } else {
+      last.push(element);
+    }
+  }
+  return chunked;
+}
+
+function chunkSlice(array, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunked;
+}
+
+module.exports = chunkSlice;
+
+/*
+chunk:
+Create empty array to hold chunks called chunked
+For each element in unchunked array
+    retrieve the last element in chunked
+    If last element does not exist, or if length is equal to chunk size
+        push a new chunk into chunked with current element
+    Else add the current element into the chunk
+
+chunkSlice:
+Create empty chunked array
+Create 'index' start at 0
+While index is less than array.length
+    Push a slice of length 'size' from 'array' into 'chunked'
+    Add 'size' to index
+*/
