66348253973f9a7fd2e17eaff69a18e8268f72e8
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2022-02-05 03:15:36 +0530

    Build Node and LinkedList class constructors.

---
+++ completed/linkedlist/index.js
@@ -0,0 +1,21 @@
+// --- Directions
+// Implement classes Node and Linked Lists
+// See 'directions' document
+
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+}
+
+const list = new LinkedList();
+list.head = new Node(10);
+
+module.exports = { Node, LinkedList };
