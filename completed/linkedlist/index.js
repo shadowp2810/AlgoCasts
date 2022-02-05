1522fbe2ab5734bfd33daa2772c2657c872a255d
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2022-02-05 18:04:30 +0530

    Refactor LinkedList with at method for first last.

--- completed/linkedlist/index.js
+++ completed/linkedlist/index.js
@@ -15,13 +15,14 @@ class LinkedList {
   }
 
   insertFirst(data) {
-    this.head = new Node(data, this.head);
+    // this.head = new Node(data, this.head);
+    this.insertAt(data, 0);
   }
 
   size() {
     let counter = 0;
     let node = this.head;
-    //if there is a head node
+
     while (node) {
       counter++;
       node = node.next;
@@ -30,17 +31,19 @@ class LinkedList {
   }
 
   getFirst() {
-    return this.head;
+    // return this.head;
+    return this.getAt(0);
   }
 
   getLast() {
-    if (!this.head) return null;
+    // if (!this.head) return null;
 
-    let node = this.head;
-    while (node) {
-      if (!node.next) return node;
-      node = node.next;
-    }
+    // let node = this.head;
+    // while (node) {
+    //   if (!node.next) return node;
+    //   node = node.next;
+    // }
+    return this.getAt(this.size() - 1);
   }
 
   clear() {
@@ -48,26 +51,28 @@ class LinkedList {
   }
 
   removeFirst() {
-    if (!this.head) return;
+    // if (!this.head) return;
 
-    this.head = this.head.next;
+    // this.head = this.head.next;
+    this.removeAt(0);
   }
 
   removeLast() {
-    if (!this.head) return;
+    // if (!this.head) return;
 
-    if (!this.head.next) {
-      this.head = null;
-      return;
-    }
+    // if (!this.head.next) {
+    //   this.head = null;
+    //   return;
+    // }
 
-    let previous = this.head;
-    let node = this.head.next;
-    while (node.next) {
-      previous = node;
-      node = node.next;
-    }
-    previous.next = null;
+    // let previous = this.head;
+    // let node = this.head.next;
+    // while (node.next) {
+    //   previous = node;
+    //   node = node.next;
+    // }
+    // previous.next = null;
+    this.removeAt(this.size() - 1);
   }
 
   insertLast(data) {
